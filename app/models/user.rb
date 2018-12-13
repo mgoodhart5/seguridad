8d97430d4d79ec28e64c51a93e1680977e83dcca
Mary Goodhart <marya.grace5@gmail.com>
2018-12-13 10:13:32 -0700

    initial commit

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+# class User < ApplicationRecord
+#
+#   # e.g., User.authenticate('penelope@turing.com', 'boom')
+#   def self.authenticate(email, password)
+#     if User.find(user[:email])
+#     # otherwise, return nil
+#   end
+# end
---
+++ spec/features/user_info_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+describe 'when a user visits /users' do
+  it 'should be able to create accounts' do
+    
+  end
+end
